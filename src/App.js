d06cb0cee6af49aed7011509ac0fc77ad0f617e0
Kevin Roan <kevinroan0303@gmail.com>
2023-10-21 21:37:11 +0530

    add notification:

--- src/App.js
+++ src/App.js
@@ -1,7 +1,22 @@
 import "./App.css";
+import { useEffect } from "react";
 import RouteLayouts from "./LayoutRoutes/RouteLayouts";
 
 function App() {
+  useEffect(() => {
+    if ("Notification" in window) {
+      Notification.requestPermission().then((permission) => {
+        if (permission === "granted") {
+          // Permission granted, you can send in-page notifications
+        }
+      });
+    }
+  }, []);
+  if ("Notification" in window && Notification.permission === "granted") {
+    const notification = new Notification("Hello Bhai", {
+      body: "Hellow Bhai You have a new Notification.",
+    });
+  }
   return (
     <div className="App">
       <RouteLayouts />
