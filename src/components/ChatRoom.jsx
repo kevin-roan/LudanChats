d33a9a52ae303a10a84f2331f964627b8aca6ee9
Kevin Roan <kevinroan0303@gmail.com>
2023-10-21 15:29:02 +0530

    form fix

--- src/components/ChatRoom.jsx
+++ src/components/ChatRoom.jsx
@@ -1,18 +1,21 @@
 import { Typography, Button } from "@material-tailwind/react";
 import { Input } from "@material-tailwind/react";
+import { useState } from "react";
 import React from "react";
-
 import { useCollectionData } from "react-firebase-hooks/firestore";
-// firestore and chats
 import firebase from "firebase/compat/app";
-
 const firestore = firebase.firestore();
 
 export default function ChatRoom() {
+  const [formData, setFormData] = useState("");
+
+  // messages fetch start
   const messagesRef = firestore.collection("messages");
   const query = messagesRef.orderBy("createdAt").limit(25);
   const [messages] = useCollectionData(query, { idField: "id" });
   console.log(messages);
+  // messages fetch end
+
   return (
     <>
       <div className=" badge badge-neutral">
@@ -23,27 +26,22 @@ export default function ChatRoom() {
           <div className="chat-bubble chat-bubble-primary m-1">Bhai jaaan</div>
         </div>
         <div className="chat chat-end">
-          <div className="chat-bubble chat-bubble-success m-1">
-            Kya Hua be ?
-          </div>
-          <div className="chat-bubble chat-bubble-success">kisne maara</div>
-        </div>
-        <div className="chat chat-end">
           {messages &&
             messages.map((msg) => (
               <div className="chat-bubble chat-bubble-success m-1">
                 {msg.text}
               </div>
             ))}
-          <div className="chat-bubble chat-bubble-success">kisne maara</div>
+          <div className="chat-bubble chat-bubble-success">{formData}</div>
         </div>
         <div className="fixed flex bottom-9 w-full">
           <input
             type="text"
-            placeholder="Type here"
+            placeholder="Say something..."
             className="input input-bordered "
+            onChange={(e) => setFormData(e.target.value)}
           />
-          <Button color="pink" className="mx-4">
+          <Button color="pink" className="mx-4" disabled={!formData}>
             Send
           </Button>
         </div>
